3813906793e9f164f5c1238ad2b41231d911ac63
SouroN21 <dasunpriyanaveen@gmail.com>
2025-05-06 23:17:05 +0530

    My new

--- backend/src/main/java/com/example/pafbackend/repositories/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.pafbackend.repositories;
-
-import com.example.pafbackend.models.Payment;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PaymentRepository extends MongoRepository<Payment, String> {
-}
--- backend/src/main/java/com/example/pafbackend/repositories/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.pafbackend.repositories;
-
-import com.example.pafbackend.models.Project;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProjectRepository extends MongoRepository<Project, String> {
-}
